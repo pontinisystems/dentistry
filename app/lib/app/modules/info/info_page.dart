08ebceda0a89e6167cf511dacbfc6082c1bf3d21
ewertonpontini <ewerton.pontini@gmail.com>
2020-04-22 19:50:49 -0300

    Refactor art XD Add layout statisct in page

--- app/lib/app/modules/info/info_page.dart
+++ app/lib/app/modules/info/info_page.dart
@@ -19,23 +19,43 @@ class _InfoPageState extends ModularState<InfoPage, InfoController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(colorThree),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              _makeHeader(),
-              SizedBox(
-                height: 5.0,
+    return Container(
+      color: Colors.white,
+      child: Scaffold(
+        backgroundColor: Color(colorThree),
+        body: Column(
+          children: <Widget>[
+            SizedBox(
+              height: MediaQuery.of(context).padding.top,
+            ),
+            _makeHeader(),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    children: <Widget>[
+                      // _makeStatistic(),
+                      Expanded(
+                        child: SingleChildScrollView(
+                          child: Container(
+                            height: MediaQuery.of(context).size.height,
+                            child: Column(
+                              children: <Widget>[
+                                Flexible(
+                                  child: _makeStatistic(),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              _makeStatistic(),
-              SizedBox(
-                height: 5.0,
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
@@ -43,13 +63,113 @@ class _InfoPageState extends ModularState<InfoPage, InfoController> {
 
   Widget _makeStatistic() {
     return Container(
-        margin: EdgeInsets.all(10),
-        height: 150.0,
-        child: Card(
-          color: Colors.white,
-          elevation: 4.0,
-          borderOnForeground: true,
-        ));
+      margin: EdgeInsets.all(10),
+      width: double.infinity,
+      height: 175.0,
+      child: Card(
+        color: Color(color_white),
+        child: 
+        
+        
+        
+        
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: 4.0,
+            ),
+            _statisctContainer(Color(color_tomato),),
+            SizedBox(
+              width: 4.0,
+            ),
+            _statisctContainer(Color(color_light_sea_green)),
+            SizedBox(
+              width: 4.0,
+            ),
+            _statisctContainer(Color(color_moon_yellow))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _statisctContainer(Color color) {
+    return Container(
+      height: 100,
+      width: 100,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.schedule,
+            color: Colors.white,
+          ),
+          SizedBox(height: 2.0,),
+          Text(
+            '10',
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 2.0,),
+
+          Text(
+            'Remarcada',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.all(Radius.circular(10))),
+    );
+  }
+
+  Widget _grafic() {
+    return Container(
+      width: 103.0,
+      height: 97.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.0),
+        gradient: LinearGradient(
+          // Where the linear gradient begins and ends
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          // Add one stop for each color. Stops should increase from 0 to 1
+          stops: [0.1, 0.9],
+          colors: [
+            // Colors are easy thanks to Flutter's Colors class.
+            Color(0xff1d83ab),
+            Color(0xff0cbab8),
+          ],
+        ),
+      ),
+      child: FlatButton(
+        child: Text(
+          'A',
+          style: TextStyle(
+            fontSize: 16.0,
+            fontFamily: 'Righteous',
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        textColor: Colors.white,
+        color: Colors.transparent,
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget _makeTest() {
+    return ListView.builder(
+        itemCount: 30,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text('data'),
+          );
+        });
   }
 
   Widget _makeHeader() {
@@ -72,7 +192,7 @@ class _InfoPageState extends ModularState<InfoPage, InfoController> {
                   backgroundColor: Colors.transparent,
                 ),
                 Container(
-                  margin: EdgeInsets.all(15),
+                  margin: EdgeInsets.all(10),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisAlignment: MainAxisAlignment.center,
--- app/lib/app/utils/colors_util.dart
+++ app/lib/app/utils/colors_util.dart
@@ -10,3 +10,22 @@ const label_color=0xFF42425B;
 
 const test_color_new_screen=0xFF9BA4B2;
 const test_color_dashBoard=0xFF95989A;
+
+
+/// todo nome das cores
+/// 
+/// 
+/// 
+const color_summer_sky =0xFF29C7D9;
+const color_moon_yellow =0xFFF3B712;
+const color_maya_blue =0xFF6A9EFF;
+const color_tomato =0xFFFF5C47;
+const color_lavender =0xFFF6F7FD;
+const color_dodger_blue=0xFF4385FF;
+const color_cherub=0xFF2D2DD;
+const color_white=0XFFFFFFFF;
+const color_light_sea_green=0XFF20CBAF;
+const color_blue_whale=0XFF142235;
+const color_light_cyan=0XFFCEE8FF;
+const color_blue_zodiac=0XFF42425B;
+
